7c09c9fdba14496ff44e389fd3bb7a9aac9da11f
Gabriel Prange <prange.gabriel@gmail.com>
2021-09-30 16:21:44 -0300

    chapter 1 done

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,14 +3,20 @@ import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
-    .then(response => response.json())
-    .then(data => setRepositories(data))
-  }, [] )
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
 
   return (
     <section className="repository-list">
@@ -18,7 +24,7 @@ export function RepositoryList() {
 
       <ul>
         {repositories.map(repository => {
-          return <RepositoryItem key={repository.name} repository={repository}/>
+          return <RepositoryItem key={repository.name} repository={repository} />
         })}
       </ul>
     </section>
